a3c41d4bcb4517b957039f96e6cc65ef30cc8087
Oksana Kich <oksanakich@gmail.com>
2019-10-18 14:14:24 +0300

    path

---
+++ pulsometer/src/js/script.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        infinite: true,
+        speed: 1300,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                dots: true,
+                arrows: false,
+              }
+            }
+          ]
+    });
+  });
