c18cf15debd78b96b9920a1a72c16091c918d5ec
Oksana Kich <oksanakich@gmail.com>
2019-11-02 13:54:47 +0200

    search form The End
    :bouquet:

--- fashion-shop/src/js/script.js
+++ fashion-shop/src/js/script.js
@@ -41,4 +41,17 @@ $(document).ready(function(){
             }
         }
     });
+
+    const iconSearch = document.querySelector('.icons__search');
+    const body = document.querySelector('body');
+  
+    iconSearch.addEventListener('click', function (event) {
+      event.stopPropagation();
+      this.classList.add('icons__search--active');
+    });
+  
+    body.addEventListener('click', function (event) {
+      iconSearch.classList.remove('icons__search--active');
+    });
+
 });
