d5e4d7f1f02a246d9d8d1f769766b8f1e21592dc
Oksana Kich <oksanakich@gmail.com>
2019-11-29 12:25:25 +0200

    bg

--- meditation/src/js/script.js
+++ meditation/src/js/script.js
@@ -71,5 +71,16 @@ $(document).ready(function(){
 
 });
 
+$(function() {
+  if ($.browser.msie && $.browser.version.substr(0,1)<7)
+  {
+    $('.tooltip').mouseover(function(){
+          $(this).children('span').show();
+        }).mouseout(function(){
+          $(this).children('span').hide();
+        })
+  }
+});
+
 
 
