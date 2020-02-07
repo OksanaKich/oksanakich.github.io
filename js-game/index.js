f70ff116ed6d16b57fc96f91184bd88e6961d6a8
Oksana Kich <oksanakich@gmail.com>
2020-02-07 15:44:57 +0200

    js game

--- js-game/index.js
+++ js-game/index.js
@@ -1,18 +1,52 @@
 var $start = document.querySelector('#start');
 var $game = document.querySelector('#game');
+var $time = document.querySelector('#time');
+var $result = document.querySelector('#result');
+var $timeHeader = document.querySelector('#time-header');
+var $resultHeader = document.querySelector('#result-header');
+
 var score = 0;
+var isGameStarted = false;
+
 
 $start.addEventListener('click', startGame);
 $game.addEventListener('click', handleBoxClick); 
 
 function startGame(){
+    isGameStarted = true;
     $game.style.backgroundColor = '#fff';
     $start.classList.add('hide');
+
+    var interval = setInterval(function(){
+        var time = parseFloat($time.textContent);
+        if (time <= 0) {
+            clearInterval(interval);
+            endGame();
+        }else{
+            $time.textContent = (time - 0.1).toFixed(1);
+        }
+    }, 100)
 
     renderBox();
 }
 
+function setGameScore() {
+    $result.textContent = score.toString();
+}
+function endGame(){
+    isGameStarted = false;
+    setGameScore();
+    $start.classList.remove('hide');
+    game.innerHTML = '';
+    $game.style.backgroundColor = '#ccc';
+    $timeHeader.classList.add('hide');
+    $resultHeader.classList.remove('hide');
+}
+
 function handleBoxClick(event){
+    if (!isGameStarted) {
+       return;
+    }
     if(event.target.dataset.box){
         score++;
         renderBox();
@@ -22,11 +56,17 @@ function handleBoxClick(event){
 function renderBox() {
     $game.innerHTML = '';
     var box = document.createElement('div');
-    box.style.height = box.style.width = '50px';
+    var boxSize = getRandom(30, 100);
+    var gameSize = $game.getBoundingClientRect();
+    var maxTop = gameSize.height - boxSize;
+    var maxLeft = gameSize.width - boxSize;
+    console.log(gameSize);
+
+    box.style.height = box.style.width = boxSize + 'px';
     box.style.position = 'absolute';
     box.style.backgroundColor = '#ccc';
-    box.style.top = '50px';
-    box.style.left = '100px';
+    box.style.top = getRandom(0, maxTop) + 'px';
+    box.style.left = getRandom(0, maxLeft) + 'px';
     box.style.cursor = 'pointer';
     box.setAttribute('data-box', 'true');
 
@@ -34,6 +74,6 @@ function renderBox() {
 }
 
 function getRandom(min, max){
-    return Math.random() * (max - min) + min
+    return Math.floor(Math.random() * (max - min) + min);
 
 }
