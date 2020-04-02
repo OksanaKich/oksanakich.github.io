00f50df9e8e11b344750c92575b549d0e3f46c20
Oksana Kich <oksanakich@gmail.com>
2020-04-02 17:33:19 +0300

    fade slider

--- slider/src/js/script.js
+++ slider/src/js/script.js
@@ -42,6 +42,35 @@ $(document).ready(function(){
     
     });  
    
+    $('.zoom__slider').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear',
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            arrows: false,
+            dots: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
+          }
+        }
+      ]
+    });
+        
      
   });
 
