a5e7e9115c70139ceee89c3a176601051c7d9800
Oksana Kich <oksanakich@gmail.com>
2020-04-15 19:44:55 +0300

    start

---
+++ start/js/main.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('.slider').slick({
+        nextArrow: '<button class="slick-arrow slick-prev"><img src="images/prev.svg" alt=""></button>',
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="images/next.svg" alt=""></button>'
+    });
+});
+
+
